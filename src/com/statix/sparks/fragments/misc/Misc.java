a1766e6d99cb9abfb356076581fdb336af8cc18b
Shubham Singh <coolsks94@gmail.com>
2018-10-31 12:38:02 -0500

    Sparks: Allow users to switch between Pie/Oreo recents [2/3]
    Change-Id: Ic65c2e5d2c7dae8527e2261af6942e3ae1c0d959
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/statix/sparks/fragments/misc/Misc.java
+++ src/com/statix/sparks/fragments/misc/Misc.java
@@ -21,10 +21,13 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import com.android.settings.R;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.du.Utils;
 import com.statix.sparks.preferences.CustomSettingsPreferenceFragment;
 
 public class Misc extends CustomSettingsPreferenceFragment {
     private static final String TAG = "Misc";
+    private ListPreference mRecentsComponentType;
+    private static final String RECENTS_COMPONENT_TYPE = "recents_component";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,13 @@ public class Misc extends CustomSettingsPreferenceFragment {
 
         addPreferencesFromResource(R.xml.misc);
 
+        // recents component type
+        mRecentsComponentType = (ListPreference) findPreference(RECENTS_COMPONENT_TYPE);
+        int type = Settings.System.getInt(resolver,
+                Settings.System.RECENTS_COMPONENT, 0);
+        mRecentsComponentType.setValue(String.valueOf(type));
+        mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
+        mRecentsComponentType.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -47,6 +57,24 @@ public class Misc extends CustomSettingsPreferenceFragment {
     @Override
     public void onPause() {
         super.onPause();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mRecentsComponentType) {
+            int type = Integer.valueOf((String) objValue);
+            int index = mRecentsComponentType.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_COMPONENT, type);
+            mRecentsComponentType.setSummary(mRecentsComponentType.getEntries()[index]);
+            if (type == 1) { // Disable swipe up gesture, if oreo type selected
+               Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
+            }
+            Utils.showSystemUiRestartDialog(getContext());
+        return true;
+        }
+        return false;
     }
 
 }
