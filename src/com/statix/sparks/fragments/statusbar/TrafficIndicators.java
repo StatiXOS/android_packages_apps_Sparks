416710b0ea10764748bf38a8d4d02b29cebea956
Alex Cruz <du.alexcruz@gmail.com>
2019-01-17 10:16:57 -0600

    Add custom features to search index [1/2]
    - Not everything will get added to the search index, for example the
    ticker fragment but we went ahead and added the necessary code to future
    proof it. If and when we get ready to add the 'ticker' or any of these
    other ones, we can just add it to searh index (second part of this commit).
    
    Change-Id: I6134091c2ec7c157ce33c0e718611592fe2431bf

--- src/com/statix/sparks/fragments/statusbar/TrafficIndicators.java
+++ src/com/statix/sparks/fragments/statusbar/TrafficIndicators.java
@@ -18,7 +18,10 @@ package com.statix.sparks.fragments.statusbar;
 
 import android.content.ContentResolver;
 import android.content.res.Resources;
+import android.content.Context;
 import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
 import android.os.UserHandle;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
@@ -26,17 +29,22 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
-import android.provider.Settings;
-
-import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
 import com.statix.support.preferences.CustomSeekBarPreference;
 
-public class TrafficIndicators extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrafficIndicators extends SettingsPreferenceFragment implements 
+	Preference.OnPreferenceChangeListener, Indexable {
 
     private CustomSeekBarPreference mThreshold;
     private SwitchPreference mNetMonitor;
@@ -85,4 +93,23 @@ public class TrafficIndicators extends SettingsPreferenceFragment implements Pre
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.SPARKS;
     }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                    final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.traffic_indicators;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    final List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+    };
 }
