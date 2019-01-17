2d6f272b76cca4e02d623cb53a946bdace7dd750
Alex Cruz <du.alexcruz@gmail.com>
2019-01-17 10:16:57 -0600

    Add custom features to search index [1/2]
    - Not everything will get added to the search index, for example the
    ticker fragment but we went ahead and added the necessary code to future
    proof it. If and when we get ready to add the 'ticker' or any of these
    other ones, we can just add it to searh index (second part of this commit).
    
    Change-Id: I6134091c2ec7c157ce33c0e718611592fe2431bf

--- src/com/statix/sparks/fragments/lockscreen/Lockscreen.java
+++ src/com/statix/sparks/fragments/lockscreen/Lockscreen.java
@@ -16,16 +16,28 @@
 
 package com.statix.sparks.fragments.lockscreen;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import com.android.settings.R;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import com.statix.sparks.preferences.CustomSettingsPreferenceFragment;
 import com.statix.support.preferences.SystemSettingListPreference;
 
-public class Lockscreen extends CustomSettingsPreferenceFragment {
+public class Lockscreen extends CustomSettingsPreferenceFragment
+        implements Indexable {
     private static final String TAG = "Lockscreen";
 
     private static final String KEY_WEATHER_TEMP = "weather_lockscreen_unit";
@@ -59,4 +71,22 @@ public class Lockscreen extends CustomSettingsPreferenceFragment {
         super.onPause();
     }
 
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                    final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.lockscreen;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    final List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+    };
 }
