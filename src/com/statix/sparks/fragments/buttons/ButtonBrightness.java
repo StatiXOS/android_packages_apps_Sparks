7445ab2383f7826d121b4982c530980733c646bc
Alex Cruz <du.alexcruz@gmail.com>
2019-01-17 16:00:06 -0600

    Search index: Only show what's available to the user
    Change-Id: Ie7a49328effc2fe1da99b2a03cd25c5d0e8b2df6

--- src/com/statix/sparks/fragments/buttons/ButtonBrightness.java
+++ src/com/statix/sparks/fragments/buttons/ButtonBrightness.java
@@ -113,7 +113,11 @@ new ArrayList<>();
                     final SearchIndexableResource sir = new 
 SearchIndexableResource(context);
                     sir.xmlResId = R.xml.button_brightness;
-                    result.add(sir);
+                    int deviceKeys = context.getResources().getInteger(
+                            com.android.internal.R.integer.config_deviceHardwareKeys);
+                    if (deviceKeys != 0) {
+                        result.add(sir);
+                    }
                     return result;
                 }
 
