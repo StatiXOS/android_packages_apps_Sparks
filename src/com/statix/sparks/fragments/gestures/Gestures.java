d926ffafe84beb25ff973041fa496bb2850a6ed4
Alex Cruz <du.alexcruz@gmail.com>
2019-01-17 10:16:57 -0600

    Add custom features to search index [1/2]
    - Not everything will get added to the search index, for example the
    ticker fragment but we went ahead and added the necessary code to future
    proof it. If and when we get ready to add the 'ticker' or any of these
    other ones, we can just add it to searh index (second part of this commit).
    
    Change-Id: I6134091c2ec7c157ce33c0e718611592fe2431bf

--- src/com/statix/sparks/fragments/gestures/Gestures.java
+++ src/com/statix/sparks/fragments/gestures/Gestures.java
@@ -16,15 +16,27 @@
 
 package com.statix.sparks.fragments.gestures;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import com.android.settings.R;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import com.statix.sparks.preferences.CustomSettingsPreferenceFragment;
 
-public class Gestures extends CustomSettingsPreferenceFragment {
+public class Gestures extends CustomSettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener, Indexable {
     private static final String TAG = "Gestures";
 
     @Override
@@ -50,4 +62,22 @@ public class Gestures extends CustomSettingsPreferenceFragment {
         super.onPause();
     }
 
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                    final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.custom_gestures;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    final List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+    };
 }
