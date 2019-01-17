8fa43c4f0ebb684fc28b3e6f8cf869dd815d0176
Alex Cruz <du.alexcruz@gmail.com>
2019-01-17 10:16:57 -0600

    Add custom features to search index [1/2]
    - Not everything will get added to the search index, for example the
    ticker fragment but we went ahead and added the necessary code to future
    proof it. If and when we get ready to add the 'ticker' or any of these
    other ones, we can just add it to searh index (second part of this commit).
    
    Change-Id: I6134091c2ec7c157ce33c0e718611592fe2431bf

--- src/com/statix/sparks/fragments/buttons/Buttons.java
+++ src/com/statix/sparks/fragments/buttons/Buttons.java
@@ -16,8 +16,11 @@
 
 package com.statix.sparks.fragments.buttons;
 
+import android.content.Context;
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
 import android.os.UserHandle;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
@@ -25,18 +28,24 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
-import android.provider.Settings;
 
-import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 import com.android.settings.smartnav.ActionFragment;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.internal.utils.ActionConstants;
 import com.android.internal.utils.ActionUtils;
 
-public class Buttons extends ActionFragment implements Preference.OnPreferenceChangeListener {
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Buttons extends ActionFragment 
+	implements Preference.OnPreferenceChangeListener, Indexable {
     private static final String TAG = "Buttons";
 
     private static final String HWKEY_DISABLE = "hardware_keys_disable";
@@ -172,5 +181,24 @@ public class Buttons extends ActionFragment implements Preference.OnPreferenceCh
     public void onPause() {
         super.onPause();
     }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                    final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.buttons;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    final List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+    };
 }
 
