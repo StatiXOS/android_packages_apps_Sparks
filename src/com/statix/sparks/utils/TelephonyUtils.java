AOSiP/platform_packages_apps_OwlsNest

Incall vibration options [3/3]
*allow setting vibration when call is connected
*allow setting vibration when call is disconnected
*allow setting vibration for call waiting
*this works with google and aosp dialer :)

Change-Id: I76838e736f960bb7e64b68bb35eef0c07028461f
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

src/com/aosip/owlsnest/utils/TelephonyUtils.java
@@ -1,0 +1,18 @@
+package com.aosip.owlsnest.utils;
+
+import android.content.Context;
+import android.telephony.TelephonyManager;
+
+public class TelephonyUtils {
+
+    private static final String TAG = TelephonyUtils.class.getSimpleName();
+
+    /**
+     * Returns whether the device is voice-capable (meaning, it is also a phone).
+     */
+    public static boolean isVoiceCapable(Context context) {
+        TelephonyManager telephony =
+                (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        return telephony != null && telephony.isVoiceCapable();
+    }
+}

StatiXOS/android_packages_apps_Sparks

src/com/statix/sparks/utils/TelephonyUtils.java
@@ -1,0 +1,19 @@
+package com.statix.sparks.utils;
+
+import android.content.Context;
+import android.telephony.TelephonyManager;
+
+public class TelephonyUtils {
+
+    private static final String TAG = TelephonyUtils.class.getSimpleName();
+
+    /**
+     * Returns whether the device is voice-capable (meaning, it is also a phone).
+     */
+    public static boolean isVoiceCapable(Context context) {
+        TelephonyManager telephony =
+                (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        return telephony != null && telephony.isVoiceCapable();
+    }
+}
+